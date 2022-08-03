f8b9b627661f854b1a1c4658a5d08b24b47a8499
Mehmet ALPEREN <me.alperen@gmail.com>
2022-08-03 13:44:03 +0300

    Project finished.

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,13 +1,16 @@
 import styled from 'styled-components'
 
 const QuoteContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
   margin: 1.5rem 0;
   background: #e5e5e5;
   padding: 0.75rem 1rem;
   border-radius: 10px;
+
+  @media screen and (min-width: 768px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
 `
 
 const Quote = ({ quote, author }) => {
--- src/pages/SingleCharacter.jsx
+++ src/pages/SingleCharacter.jsx
@@ -1,6 +1,34 @@
 import { useParams } from 'react-router-dom'
 import { useGlobalContext } from '../context'
 import { useEffect } from 'react'
+import styled from 'styled-components'
+
+const SingleCharacterContainer = styled.div`
+  @media screen and (min-width: 768px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+  }
+`
+
+const SingleCharacterImg = styled.img`
+  max-width: 25rem;
+  max-height: 30rem;
+`
+
+const ImageContainer = styled.div``
+
+const SingleCharacterInfo = styled.div`
+  margin-top: 2rem;
+`
+
+const SingleCharacterInfoDetail = styled.p`
+  span {
+    font-weight: 500;
+  }
+`
 
 const SingleCharacter = () => {
   const { char_id } = useParams()
@@ -12,7 +40,35 @@ const SingleCharacter = () => {
     getSingleCharacter(char_id)
   }, [char_id])
 
-  return <div>{singleCharacter[0]?.name}</div>
+  return (
+    <SingleCharacterContainer>
+      <ImageContainer>
+        <SingleCharacterImg src={singleCharacter[0]?.img} alt="img" />
+      </ImageContainer>
+      <SingleCharacterInfo>
+        <SingleCharacterInfoDetail>
+          Name: <span>{singleCharacter[0]?.name}</span>
+        </SingleCharacterInfoDetail>
+        <SingleCharacterInfoDetail>
+          Nickname: <span>{singleCharacter[0]?.nickname}</span>
+        </SingleCharacterInfoDetail>
+        <SingleCharacterInfoDetail>
+          Portrayed: <span>{singleCharacter[0]?.portrayed}</span>
+        </SingleCharacterInfoDetail>
+        <SingleCharacterInfoDetail>
+          Birthday: <span>{singleCharacter[0]?.birthday}</span>
+        </SingleCharacterInfoDetail>
+        <SingleCharacterInfoDetail>
+          Occupation:
+          <span>
+            {singleCharacter[0]?.occupation.map((item) => {
+              return <span> {item}</span>
+            })}
+          </span>
+        </SingleCharacterInfoDetail>
+      </SingleCharacterInfo>
+    </SingleCharacterContainer>
+  )
 }
 
 export default SingleCharacter
